66420e50481ab314a6ccd8e2e544053049040b53
Barnay111 <kystaubaev.iliyas1101@mail.ru>
2025-10-31 05:25:03 +0500

    Add files via upload

---
+++ FirstProject/Lesson2/Task3.dart
@@ -0,0 +1,8 @@
+void main(){
+String name = 'Ғұбайдулла';
+  int currentYear = 2025;
+  int birthYear = 2005;
+  int age = currentYear - birthYear;
+
+  print("Сәлем, $name! Саған $age жас екен.");
+}
---
+++ FirstProject/Lesson3/Task1.dart
@@ -0,0 +1,6 @@
+void main(){
+   int a = 12;
+  int b = 5;
+
+  print("a + b = ${a+b}\na - b = ${a-b}\na * b = ${a*b}\na / b = ${a/b}\na ~/ b = ${a~/b}\na % b = ${a%b}");
+}
